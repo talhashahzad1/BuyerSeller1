0012db6bc14609c6df10a4be7993576e2c9fde89
talhashahzad1 <talhashahzad@975.com>
2020-08-13 12:25:23 +0500

    Add project files.

---
+++ BuyerSeller/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BuyerSeller.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
